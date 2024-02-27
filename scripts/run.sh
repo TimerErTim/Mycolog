c2221bd0d44c7df1aa5196125fd619424faa3408
TimerErTim <timerertim@gmail.com>
2024-02-27 18:34:02 +0100

    Add run script for exported app

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p log; touch log/total.log
+
+# This assumes your working directory is inside the build folder
+
+until ./mycolog 2>&1 | tee -a log/total.log | tspin --bucket-size 0; do
+    sleep 5
+done
