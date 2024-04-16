45430e36987f9bb167f5f7f09027b61a8259dfc8
TimerErTim <timerertim@gmail.com>
2024-04-16 15:30:08 +0200

    Add config file to export script

--- scripts/export.sh
+++ scripts/export.sh
@@ -7,7 +7,7 @@ cd "$scriptdir" && cd ../ || exit;
 scripts/build_backend.sh \
 && mkdir -p build/ \
 && cp backend/target/release/mycolog build/ \
-&& cp -r scripts/run.sh working_dir/secrets working_dir/migrations working_dir/schedules build/;
+&& cp -r scripts/run.sh  working_dir/config working_dir/secrets working_dir/migrations working_dir/schedules working_dir/emails build/;
 scripts/build_frontend.sh \
 && mkdir -p build/site/ \
 && cp -r frontend/build/* build/site/;
