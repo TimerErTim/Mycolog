758d3023f7aeaeb4b0d79968dfaa19afe3e782cf
TimerErTim <timerertim@gmail.com>
2024-05-22 00:47:39 +0200

    Change run script to wrap executable

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir -p log; touch log/total.log
-
 # This assumes your working directory is inside the build folder
-
-until ./mycolog "$@" 2>&1 | tee -a log/total.log | tspin; do
-    sleep 5
-done
+exec ./mycolog "$@";
