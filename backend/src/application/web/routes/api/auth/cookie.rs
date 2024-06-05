c7adf8407f1e69fca7d8cdb93438b6473786ebb3
TimerErTim <timerertim@gmail.com>
2024-06-05 21:11:13 +0200

    Make auth cookie more accessable in dev mode

--- backend/src/application/web/routes/api/auth/cookie.rs
+++ backend/src/application/web/routes/api/auth/cookie.rs
@@ -8,9 +8,14 @@ pub fn build_auth_cookie(token: AuthToken, remember: bool) -> Cookie<'static> {
     let mut builder = Cookie::build(("auth", token.to_insecure()))
         .secure(true)
         .http_only(true)
-        .same_site(SameSite::Strict)
         .path("/api")
-        .domain("mycolog.net");
+        .same_site(SameSite::None);
+
+    // Disable domain specification for dev env
+    if cfg!(not(feature = "dev-env")) {
+        builder = builder.domain("mycolog.net").same_site(SameSite::Strict);
+    }
+
     if remember {
         builder = builder.max_age(Duration::from_days(30).try_into().unwrap());
     }
